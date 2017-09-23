569a6f8fae142e14518ec443c03a9f4475e560c5
Sahil Ambardekar <ambardekarsahil@gmail.com>
2017-09-22 23:11:22 -0400

    added pods, walmart api file

---
+++ Pods/Target Support Files/Pods-Venta/Pods-Venta-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Venta : NSObject
+@end
+@implementation PodsDummy_Pods_Venta
+@end
---
+++ Venta/WalmartAPI.swift
@@ -0,0 +1,18 @@
+//
+//  WalmartAPI.swift
+//  Venta
+//
+//  Created by Sahil Ambardekar on 9/22/17.
+//  Copyright © 2017 mhacksx. All rights reserved.
+//
+
+import Foundation
+
+let api = WalmartAPI()
+
+struct WalmartAPI {
+    let defaultAPI = {
+        return api
+    }
+    let baseURL = ""
+}
